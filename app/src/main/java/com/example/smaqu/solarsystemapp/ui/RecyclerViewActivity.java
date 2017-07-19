ad31d9ee3df778c8d15dabb09635dc231a4d8aff
SmaQu <maciej.smolik95@gmail.com>
2017-07-19 20:09:59 +0200

    Version 1.0.2-menu,dialog,radiogroup-0%

--- app/src/main/java/com/example/smaqu/solarsystemapp/ui/RecyclerViewActivity.java
+++ app/src/main/java/com/example/smaqu/solarsystemapp/ui/RecyclerViewActivity.java
@@ -1,10 +1,14 @@
 package com.example.smaqu.solarsystemapp.ui;
 
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 
 import com.example.smaqu.solarsystemapp.R;
@@ -63,4 +67,33 @@ public class RecyclerViewActivity extends AppCompatActivity implements RecyclerV
         startActivity(intent);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_sort, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            AlertDialog.Builder mBulider = new AlertDialog.Builder(RecyclerViewActivity.this);
+            View mView = getLayoutInflater().inflate(R.layout.dialog_sort_recyclerview,null);
+
+            mBulider.setView(mView);
+            AlertDialog dialog = mBulider.create();
+            dialog.show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
